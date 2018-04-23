d87f157636c01658afcdd67990cbc3739ee3a42c
Anthony Lefebvre-Allen <th3antsman@gmail.com>
2018-04-23 12:49:12 +1200

    Updated the readme to cover more of the installation and usage instructions. And update the server.js code to work more smoothly and across different servers/host more easily.

--- README.md
+++ README.md
@@ -1,3 +1,69 @@
-URL Shortner Microservice
-**************************
+# URL Shortner Microservice
+-----------------------
+
 Microservice that handles shortening url's. Can take in a long url and genrate a short code for it and store it in a db for future use. Or get given a short code and redirect to the original url that the short code was made for.
+
+-----------------------
+
+This is built to be used with glitch and an example of the microservice can be found [Here](https://spring-bubble.glitch.me/)
+
+-----------------------
+
+### Installation
+
+The microservice consists of a single file called server.js.
+
+To setup the microservice the repository can be cloned, and then run
+
+```
+npm install
+```
+This will install the required dependencies for the microservice.
+
+###### Database
+
+MongoDB is the database used to store the URL data for future lookup of the long and short URLs'.
+
+The database URL is stored in a .env file so that it can be private from repo and can be more easily changed between servers.
+
+The URL can is added in the form:
+```
+MONGOURI="Address of your mongodb"
+```
+
+###### Running the Microservice
+
+The microservice can then be run using either of the following commands:
+
+```javascript
+PORT=XXXX npm start
+```
+
+OR
+
+```javascript
+PORT=XXXX node server.js
+```
+
+Where __XXXX__ is the port number you want it to run on.
+
+-----------------------
+
+##### Usage
+
+To generate a shortend URL you can use the following path on either your webiste of the one in the given example:
+
+```URL
+https://spring-bubble.glitch.me/new/https://www.google.com
+```
+
+This tells the microservice to create a short url for ```https://www.google.com```. The microservice will then give back a JSON object with both the original url and short url.
+It will also check if the short url already exist in the DB so that duplicates aren't created.
+
+Passing the short code, shown as follows:
+
+```URL
+https://spring-bubble.glitch.me/YYYY
+```
+
+Where ```YYYY``` is the short code for the original website. This will execute a redirect to the original url that was given, or if the url doesn't exisit in the database will give a warning message saying so.
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-// server.js
-// where your node app starts
+// URL Shortner Microservice
 
-// init project
+// Microservice that takes a "normal" URL and create a short URL for it,
+// that can then be used to redirect when the short-url is requested.
 const express = require('express');
 const mongodb = require('mongodb');
 const validUrl = require('valid-url');
 const shortid = require('shortid');
+var dotenv = require('dotenv');
+
+dotenv.load(); // Load the environment variables from the .env
+
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 const mongoURL = process.env.MONGOURI;
@@ -21,62 +21,74 @@ app.get("/", (request, response) => {
     response.end("URL Shortener Microservice");
 });
 
+// Route for handling getting a short code.
 app.get('/:short', (request, response) => {
-  MongoClient.connect(mongoURL, function (err, db) {
-    if (err) {
-      console.log("Error connecting to the mongodb server");
-    } else {
-      // var db = client.db('sandboxdeployment');
-      var collection = db.collection('tinylinks');
-      var shortcode = "https://spring-bubble.glitch.me/" + (request.params.short);
-      collection.findOne({"short_url": shortcode }, (err, doc) => {
-        if (doc != null) {
-          response.redirect(doc.original_url);
+    MongoClient.connect(mongoURL, function(err, db) {
+        if (err) {
+            console.log("Error connecting to the mongodb server");
         } else {
-          response.json({"Error": "Short code DNE in database"})
+            var collection = db.collection('tinylinks');
+            // var shortcode = "https://spring-bubble.glitch.me/" + (request.params.short);
+            var shortcode = (request.params.short);
+
+            // Lookup the given short url in the DB, and redirect to the normal url if
+            // it is found. Otherwise the short url is invalid and DNE in the DB.
+            collection.findOne({
+                "short_url": shortcode
+            }, (err, doc) => {
+                if (doc != null) {
+                    response.redirect(doc.original_url);
+                    db.close();
+                } else {
+                    response.json({"Error": "Short code DNE in database"})
+                    db.close();
+                }
+            });
         }
-      });
-    }
-    db.close();
-  })
+    })
 });
 
+// Route for handling making a shortcode from a "normal" URL
 app.get('/new/:original(*)', (request, response) => {
-  MongoClient.connect(mongoURL, function (err, db) {
-    if (err) {
-        console.log("Error connecting to mongodb server")
-    } else {
-      // var db = client.db('sandboxdeployment');
-      var collection = db.collection('tinylinks');
-      var givenUrl = request.params.original;
-      if (validUrl.isUri(givenUrl)) {
-        console.log(givenUrl);
-        collection.findOne({"original_url": givenUrl}, {"short_url": 1, "_id": 0}, (err, doc) => {
-          if (doc != null) {
-            // If there is currently a document in the DB with the orignal url, give back the short code for it.
-            response.json({
-              "original_url": givenUrl,
-              "short_url": doc.short_url
-            });
-          } else {
-            // If the orignal URL doesn't exist in the DB, create it.
-            collection.insert([{original_url: givenUrl, short_url: ("https://spring-bubble.glitch.me/" +shortid.generate())}]);
-            response.json({
-              "original_url": givenUrl,
-              "short_url": ("https://spring-bubble.glitch.me/" +shortid.generate())
-            });
-          }
-        })
-        // response.end();
-      } else {
-        response.json({"Error": "Invalid URL"})
-      }
-    }
-    db.close();
-  })
+    MongoClient.connect(mongoURL, function(err, db) {
+        if (err) {
+            console.log("Error connecting to mongodb server")
+        } else {
+            var collection = db.collection('tinylinks');
+            var givenUrl = request.params.original;
+            if (validUrl.isUri(givenUrl)) {
+                collection.findOne({
+                    "original_url": givenUrl
+                }, {
+                    "short_url": 1,
+                    "_id": 0
+                }, (err, doc) => {
+                    if (doc != null) {
+                        // If there is currently an entry for the orignal url, give back the short code for it.
+                        response.json({"original_url": givenUrl, "short_url": doc.short_url});
+                        db.close();
+                    } else {
+                        // If the orignal URL doesn't exist in the DB, create it.
+                        var shortUrl = shortid.generate();
+                        collection.insert([
+                            {
+                                original_url: givenUrl,
+                                short_url: shortUrl
+                            }
+                        ]);
+                        response.json({"original_url": givenUrl, "short_url": shortUrl});
+                        db.close();
+                    }
+                });
+            } else {
+                response.json({"Error": "Invalid URL"})
+                db.close();
+            }
+        }
+    })
 });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
+    console.log(`Your microservice is listening on port ${listener.address().port}`)
 });
